1293e712cfda7c72c05653074bbccdff84ded166
Roberto Gomez <mail@robertogomezjr.com>
2015-02-07 16:15:06 -0800

    Added create_resource_obj() function

--- ypb.py
+++ ypb.py
@@ -62,14 +62,8 @@ def backup_playlists(playlists_request):
 
 # Creates the initial playlists request used in backup_playlists()
 # Checks the commandline arguments and assembles the correct request
-# Sets up OAuth 2.0 for authorized requests if necessary
 def setup_request(options):
-    global youtube
-
     if (options.channelid):
-        youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-            developerKey=DEVELOPER_KEY)
-
         request = youtube.playlists().list(
             part="id,snippet",
             fields="items(id,snippet/title),nextPageToken,pageInfo/totalResults",
@@ -77,9 +71,6 @@ def setup_request(options):
             maxResults=50
         )
     elif (options.youtube_username):
-        youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-            developerKey=DEVELOPER_KEY)
-
         # Create channel request to obtain channel id from YouTube username
         channel_request = youtube.channels().list(
             part="id",
@@ -99,6 +90,24 @@ def setup_request(options):
         except IndexError:
             sys.exit("No channel found for {}".format(options.youtube_username))
     else:
+        request = youtube.playlists().list(
+            part="id,snippet",
+            fields="items(id,snippet/title),nextPageToken,pageInfo/totalResults",
+            mine="true",
+            maxResults=50
+        )
+
+    return request
+
+# Creates the resource object for interacting with the YouTube API
+# Sets up OAuth 2.0 for authorized requests if necessary
+def create_resource_obj(options):
+    global youtube
+
+    if (options.channelid or options.youtube_username):
+        youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+            developerKey=DEVELOPER_KEY)
+    else:
         flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
             message=MISSING_CLIENT_SECRETS_MESSAGE,
             scope=YOUTUBE_READONLY_SCOPE)
@@ -112,15 +121,6 @@ def setup_request(options):
         youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
             http=credentials.authorize(httplib2.Http()))
 
-        request = youtube.playlists().list(
-            part="id,snippet",
-            fields="items(id,snippet/title),nextPageToken,pageInfo/totalResults",
-            mine="true",
-            maxResults=50
-        )
-
-    return request
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -133,6 +133,7 @@ if __name__ == "__main__":
     youtube = None
 
     try:
+        create_resource_obj(args)
         req = setup_request(args)
         backup_playlists(req)
     except HttpError, e:
